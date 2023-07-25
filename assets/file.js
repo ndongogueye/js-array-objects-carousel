57c9bc8a9e3b48c5b97eb6b3765bc18ade3717fe
ndongogueye <bayeg2003@gmail.com>
2023-07-25 12:08:00 +0200

    html-css

---
+++ assets/file.js
@@ -0,0 +1,41 @@
+
+
+const images = [
+    {
+      src: "img/01.webp",
+      alt: "Spiderman",
+      title: "Spiderman",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.",
+    },
+    {
+      src: "img/02.webp",
+      alt: "Ratchet & Clank",
+      title: "Ratchet & Clank",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.",
+    },
+    {
+      src: "img/03.webp",
+      alt: "Fortnite",
+      title: "Fortnite",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.",
+    },
+    {
+      src: "img/04.webp",
+      alt: "Stray",
+      title: "Stray",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.",
+    },
+    {
+      src: "img/05.webp",
+      alt: "Avengers",
+      title: "Avengers",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.",
+    },
+  ];
+  
+  
